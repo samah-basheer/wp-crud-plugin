678e12ee8f0028540f5bcf85e3d6d17dd7dc948e
samah-basheer <samahssb2001@gmail.com>
2022-08-11 23:55:27 +0300

    added submenu

--- admin/menu.php
+++ admin/menu.php
@@ -2,15 +2,23 @@
 
 function crud_admin_menu() {
 
-    add_menu_page('CRUD Action','CRUD Action', 'activate_plugins', 'crud', 'crud_page', 'dashicons-admin-users', 63 );
+    add_menu_page('CRUD Action','CRUD Action', 'activate_plugins', 'crud', 'crud_page', 'dashicons-format-gallery', 63 );
 
     add_submenu_page(
         'crud',
-        'CRUD Action',
-        'CRUD Action',
+        'Subscribers',
+        'All Subscribers',
         'edit_pages',
         'crud',
         'crud_page'
+    );
+    add_submenu_page(
+        'crud',
+        'Add New Subscriber',
+        'Add New',
+        'edit_pages',
+        'subscriber_new',
+        'add_page'
     );
     // add_options_page puts a menu/link in the “Settings” menu
     // add_menu_page puts a menu/link at the same level as “Dashboard”, “Posts”, “Media”, etc.
---
+++ admin/pages/add_new.php
@@ -0,0 +1,8 @@
+<?php
+function add_page() {
+    ?>
+    <div class="wrap">
+        <h1 class="wp-heading-inline"><?php echo esc_html( get_admin_page_title() ); ?></h1>
+    </div>
+    <?php
+}
