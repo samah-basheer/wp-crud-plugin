b546a4d2d79da03378d5e609ce975db77007599a
samahsb <samahssb2001@gmail.com>
2022-08-11 23:42:55 +0300

    added plugin setup with menu

---
+++ admin/pages/crud_page.php
@@ -0,0 +1,8 @@
+<?php
+function crud_page() {
+    ?>
+    <div class="wrap">
+        <h1 class="wp-heading-inline"><?php echo esc_html( get_admin_page_title() ); ?></h1>
+    </div>
+    <?php
+}
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: CRUD Plugin
+ * Description: A plugin for crud action
+ * Version: 1.0.0
+ * Author: Samah Basheer
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
+
+function codechief_settings_page($links) {
+    $links[] = '<a href="/wp-admin/admin.php?page=crud">' . __( 'Settings', 'domain' ) . '</a>';
+    return $links;
+}
+add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'codechief_settings_page');
+
+include dirname( __FILE__) . '/admin/menu.php';
+include dirname( __FILE__) . '/admin/pages/crud_page.php';
