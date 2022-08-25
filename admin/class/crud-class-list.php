26292d37be4e7f38b7bc0e4e950f43c0061f0c7d
samah-basheer <samahssb2001@gmail.com>
2022-08-25 22:02:48 +0300

    change edit url

--- admin/class/crud-class-list.php
+++ admin/class/crud-class-list.php
@@ -92,7 +92,7 @@ class Crud_List_Table extends WP_List_Table {
     }
     function column_name($item) {
         $actions = array(
-            'edit'      => sprintf('<a href="?page=%s&action=%s&id=%s">Edit</a>',$_REQUEST['page'],'edit',$item['id']),
+            'edit'      => sprintf('<a href="?page=%s&action=%s&subscriber=%s">Edit</a>',$_REQUEST['page'],'edit',$item['id']),
             'delete'    => sprintf('<a href="#" onclick="delete_subscriber(%s)">Delete</a>',$item['id']),
         );
 
